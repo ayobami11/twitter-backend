211557c6c38f046d02a1274eb356c1379bfdf26f
Tunwase Ayobami <tunwaseayobami98@gmail.com>
2022-06-22 15:04:26 +0100

    Bug fixes

--- src/controllers/user/logoutUser.js
+++ src/controllers/user/logoutUser.js
@@ -6,8 +6,8 @@ const logout = async (req, res) => {
 
         await User.findByIdAndUpdate(userId, { accessToken: '', refreshToken: '' });
 
-        res.clearCookie('accessToken', { secure: NODE_ENV !== 'development' });
-        res.clearCookie('refreshToken', { secure: NODE_ENV !== 'development' });
+        res.clearCookie('accessToken', { secure: NODE_ENV !== 'development', sameSite: 'None', httpOnly: true });
+        res.clearCookie('refreshToken', { secure: NODE_ENV !== 'development', sameSite: 'None', httpOnly: true });
 
         return res.status(200).json({ success: true, message: 'Logout successful.' });
     } catch (error) {
