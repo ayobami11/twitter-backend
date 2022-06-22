6c3d97ffb6c477ef93b6363e21b19da9adebd5b8
Tunwase Ayobami <tunwaseayobami98@gmail.com>
2022-06-22 15:07:20 +0100

    Bug fixes

--- src/controllers/user/logoutUser.js
+++ src/controllers/user/logoutUser.js
@@ -1,13 +1,15 @@
 const User = require('../../models/user');
 
+const { NODE_ENV } = process.env;
+
 const logout = async (req, res) => {
     try {
         const { userId } = req.user;
 
         await User.findByIdAndUpdate(userId, { accessToken: '', refreshToken: '' });
 
-        res.clearCookie('accessToken', { secure: NODE_ENV !== 'development', sameSite: 'None', httpOnly: true });
-        res.clearCookie('refreshToken', { secure: NODE_ENV !== 'development', sameSite: 'None', httpOnly: true });
+        res.clearCookie('accessToken', { secure: NODE_ENV !== 'development' });
+        res.clearCookie('refreshToken', { secure: NODE_ENV !== 'development' });
 
         return res.status(200).json({ success: true, message: 'Logout successful.' });
     } catch (error) {
